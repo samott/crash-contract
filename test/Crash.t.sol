cf8e0da131ee3956cab74361492f9d34bb9dfe15
Shaun Amott <shaun@inerd.com>
2024-06-09 00:04:47 +0200

    Add initial (empty) tests.

---
+++ test/Crash.t.sol
@@ -0,0 +1,12 @@
+// Crash game - treasury/deposit contract
+//
+// Author: Shaun Amott <shaun@inerd.com>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Crash} from "../src/Crash.sol";
+
+contract CrashTest is Test {}
